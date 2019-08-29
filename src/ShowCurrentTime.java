a4ce01d77560f29c62bedbb02f6c93354d802147
sighrus <sighrusyan@gmail.com>
2019-08-29 04:24:51 -0400

    august 29 4:24am

---
+++ src/ShowCurrentTime.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class ShowCurrentTime {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the time zone offset to GMT: ");
+        int offset = input.nextInt();
+
+        // Obtain the total milliseconds since midnight, Jan 1, 1970
+        long totalMilliseconds = System.currentTimeMillis();
+
+        // Obtain the total seconds since midnight, Jan 1, 1970
+        long totalSeconds = totalMilliseconds / 1000;
+
+        // Compute the current second in the minute in the hour
+        long currentSecond = totalSeconds % 60;
+
+        // Obtain the total minutes
+        long totalMinutes = totalSeconds / 60;
+
+        // Compute the current minute in the hour
+        long currentMinute = totalMinutes % 60;
+
+        // Obtain the total hours
+        long totalHours = totalMinutes / 60;
+
+        // Compute the current hour
+        long currentHour = totalHours % 24;
+
+        if (currentHour + offset < 0) {
+            currentHour += offset + 12;
+        } else if (currentHour + offset > 0 && currentHour + offset < 12) {
+            currentHour += offset;
+        } else if (currentHour + offset > 12) {
+            currentHour += offset - 12;
+        } else if (currentHour + offset == 0) {
+            currentHour = 0;
+        } else {
+            currentHour = 0;
+        }
+
+        // Display results
+
+        System.out.println("Current time is " + currentHour + ":"
+               + currentMinute + ":" + currentSecond + " GMT");
+        }
+}
--- src/chapter2.java
+++ src/chapter2.java
@@ -1,21 +1,51 @@
 import java.util.Scanner;
 
 public class chapter2 {
+
+    static final Scanner input = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         ch2Q5();
+        ch2Q6();
+        ch2Q7();
 
 
     }
 
     public static void ch2Q5() {
-        Scanner input = new Scanner(System.in);
         System.out.println("Enter the subtotal and a gratuity rate: ");
         int sub = input.nextInt();
         int grat = input.nextInt();
         System.out.println("The gratuity is $" + sub*grat/100.0 + " and the total is $" + (sub + (sub*grat/100.0)));
     }
-    //test 2
+
+    public static void ch2Q6() {
+        System.out.println("Enter a number between 0 and 1000: ");
+        int n = input.nextInt();
+        if (n >= 0 && n <= 1000) {
+            int sum = 0;
+            while (n != 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            System.out.println("The sum of the digits is " + sum);
+        } else
+            System.out.println("Number is not within range of 0 and 1000");
+    }
+
+    public static void ch2Q7() {
+        System.out.println("Enter the number of minutes: ");
+        int min = input.nextInt();
+        int years = min / (365 * 24 * 60);
+        int remDays = (min / (24 * 60)) % 365;
+        System.out.println(min + " minutes is approximately " + years + " years and " + remDays + " days");
+
+    }
+
+    public static void ch2Q8() {
+
+    }
 
 
 }
